00d4b483ae28812e11055d40b69faf6afd081955
tunacasserole <aaron@highergroundsolutions.com>
2016-05-03 07:49:31 -0600

    stuff

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
 Rails.application.config.assets.paths << Rails.root.join("app", "assets", "vendors")
 # Adding Webfonts to the Asset Pipeline
-config.assets.precompile << Proc.new { |path|
+Rails.application.config.assets.precompile << Proc.new { |path|
   if path =~ /\.(eot|svg|ttf|woff)\z/
     true
   end
