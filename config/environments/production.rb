5f2157297bfe67b98250de42eeb13a2f2d86e72f
tunacasserole <aaron@highergroundsolutions.com>
2016-05-05 17:10:18 -0600

    bindit enhancements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Rails.application.configure do
     password: "CrossFit"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "bindit.io" }
+  config.action_mailer.default_url_options = { :host => "cloud.bindit.io" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
