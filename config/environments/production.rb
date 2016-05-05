922839c2f3e489ba0afe5973dddec68f8add7f99
tunacasserole <aaron@highergroundsolutions.com>
2016-05-05 17:05:46 -0600

    bindit enhancements

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,14 +74,14 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "buildit.io",
+    domain: "gmail.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "aaron@buildit.io",
     password: "CrossFit"
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => "buildit.io" }
+  config.action_mailer.default_url_options = { :host => "bindit.io" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
